9d1b732218569393067020f813f5c5b3abf7b490
pavan-code <pavankumaranguluri1@gmail.com>
2021-10-24 23:54:44 +0530

    Revert "Revert "Update Stashfile.java""
    This reverts commit 38eeda9b406a71904adf965ca97bdd276f07bee8.

--- src/main/java/com/example/git/surya/Stashfile.java
+++ src/main/java/com/example/git/surya/Stashfile.java
@@ -3,5 +3,6 @@ package com.example.git.surya;
 public class Stashfile {
 	public static void main(String[] args) {
 		System.out.println("in stashed file");
+		System.out.println("checking out pull command");
 	}
 }

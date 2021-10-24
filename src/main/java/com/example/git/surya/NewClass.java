b797a124bea5c794117bd6a3c968382eeaf5616d
pavan-code <pavankumaranguluri1@gmail.com>
2021-10-24 16:26:15 +0530

    rename

--- src/main/java/com/example/git/surya/NewClass.java
+++ src/main/java/com/example/git/surya/NewClass.java
@@ -3,5 +3,6 @@ package com.example.git.surya;
 public class NewClass {
 	public static void main(String[] args) {
 		System.out.println("in new class");
+		System.out.println("testing remote rename");
 	}
 }
